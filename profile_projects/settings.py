dc3f86f767ff056a931397b5703007aeac6e5e3d
Arshit <f20220331@pilani.bits-pilani.ac.in>
2023-08-01 23:24:24 +0530

    added custom user profile model, manager and migrations

--- profile_projects/settings.py
+++ profile_projects/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
